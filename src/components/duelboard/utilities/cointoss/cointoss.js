2e1e0ec2a1eaa5759b7d297e24e69418b5623ca0
hunterjr88 <dsanders_m633@outlook.com>
2018-12-23 22:04:44 -0500

    created cointoss function

--- src/components/duelboard/utilities/cointoss/cointoss.js
+++ src/components/duelboard/utilities/cointoss/cointoss.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class Cointoss extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { 
+            cointoss: "Heads or Tails?"
+        };
+    }
+
+  //handles the coin toss
+  handleCoinToss = () => {
+    let tossValue = (Math.floor(Math.random() * 2) === 0) ? 'Heads' : 'Tails';
+    this.setState({
+        cointoss: tossValue + " Wins!"
+    })
+  }  
+
   render() {
     return (
         <div id="CointossContainer" className="row mt-4 pt-4 d-flex justify-content-center">
@@ -8,17 +24,22 @@ class Cointoss extends Component {
                 <div className="row">
                     <div className="col-12 d-flex justify-content-center">
                         <div className="d-flex align-items-end">
-                        <p><i className="far fa-copyright"></i></p>
+                            <p><i className="far fa-copyright"></i></p>
+                        </div>
+                    </div>
+                    <div className="mt-4 pt-4 col-12 d-flex justify-content-center">
+                        <div className="d-flex align-items-end">
+                            <p>{this.state.cointoss}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="mt-4 pt-4 col-8 d-flex justify-content-center">
+            <div className="pt-4 col-8 d-flex justify-content-center">
               <div className="coin-controls row d-flex justify-content-center">
                   <div className="col-3 d-flex justify-content-center align-items-center">
                     <p>+</p>
                   </div>
-                  <div className="col-6 d-flex justify-content-center align-items-center">
+                  <div onClick = {() => this.handleCoinToss()} className="col-6 d-flex justify-content-center align-items-center">
                     <p>TOSS</p>
                   </div>
                   <div className="col-3 d-flex justify-content-center align-items-center">
