67cb107982cb4ef7327fbaf0e03629aa45b08631
hunterjr88 <dsanders_m633@outlook.com>
2018-12-23 21:48:05 -0500

    replaced settings icon with fullscreen toggle

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+  ToggleFullscreen = () => {
+    var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+        (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+        (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+        (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+    var docElm = document.documentElement;
+    if (!isInFullScreen) {
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        } else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        } else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        } else if (docElm.msRequestFullscreen) {
+            docElm.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
   render() {
     return (
       <header className="row align-self-start">
@@ -8,7 +39,7 @@ class Header extends Component {
             <p>DuelCalculator.js</p>
         </div>
         <div className="col-8 pt-4 d-flex justify-content-end">
-            <p><i className="fas fa-cog"></i></p>
+            <p><i onClick={() => this.ToggleFullscreen() } class="fas fa-expand"></i></p>
         </div>
       </header>
     );
