ba665d8da1a4fd6cb26bf57b21eaf76d8ed5b9d8
hunterjr88 <dsanders_m633@outlook.com>
2018-12-28 01:12:19 -0500

    added background animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.scss';
 import Header from './components/header/header.js';
 import Duelboard from './components/duelboard/duelboard.js';
@@ -16,6 +17,52 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid app-container">
+        <Particles 
+        className="particles"
+        params={{
+          "particles": {
+              "number": {
+                  "value": 60,
+                  "density": {
+                      "enable": true,
+                      "value_area": 1500
+                  }
+              },
+              "line_linked": {
+                  "enable": true,
+                  "opacity": 0.02
+              },
+              "move": {
+                  "direction": "right",
+                  "speed": 0.30
+              },
+              "size": {
+                  "value": 1.5
+              },
+              "opacity": {
+                  "anim": {
+                      "enable": true,
+                      "speed": 1,
+                      "opacity_min": 0.05
+                  }
+              }
+          },
+          "interactivity": {
+              "events": {
+                  "onclick": {
+                      "enable": true,
+                      "mode": "push"
+                  }
+              },
+              "modes": {
+                  "push": {
+                      "particles_nb": 1
+                  }
+              }
+          },
+          "retina_detect": true
+      }}
+        />
         <Header />
         <Duelboard />
         <Footer />
--- src/components/duelboard/duelboard.js
+++ src/components/duelboard/duelboard.js
@@ -94,10 +94,16 @@ class Duelboard extends Component {
     Velocity(document.getElementById("UtilityBar"),{ opacity: [1,0], scale: [1, 1]},{duration:300,delay:300});
     
   }
+
+  backgroundAnimation = () => {
+    // particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"","width":230,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":false,"mode":"bubble"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+    return false;
+  }
   
   componentDidMount(){
     
     this.resetEffect();
+    this.backgroundAnimation();
     
   }
   componentDidUpdate(){
@@ -117,6 +123,8 @@ class Duelboard extends Component {
       if(this.state.view === "mainboard"){
           return (
             <div id="DuelBoardContainer" refs="block" className="row d-flex align-items-end">
+            
+            {/* <div id="particles-js"></div> */}
               <div className="col-12">
                 <div className="row d-flex justify-content-around">
                     <Players 
